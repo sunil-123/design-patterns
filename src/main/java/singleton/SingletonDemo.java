454f8e5c3906414c36216c465828480f243c4a92
Sunil Kumar <sunilk@exzeo.com>
2015-06-27 06:19:15 -0400

    Implemented Singleton Desing Pattern

---
+++ src/main/java/singleton/SingletonDemo.java
@@ -0,0 +1,8 @@
+package main.java.singleton;
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+        SingleObject object = SingleObject.getInstance();
+        object.showMessage();
+    }
+}
